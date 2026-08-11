43140aee48c2ee674cbc29f8c08744f8887d11d6
ij206050-arch <ij206050@gmail.com>
2026-08-11 23:23:09 +0100

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: { extend: { colors: { brand: "#16A34A", brandDark: "#0F7A34" } } },
-  plugins: [],
-};
